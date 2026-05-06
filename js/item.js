f4aecd2dee8abd80d7e73112e477107e42b60468
rommel dalisay <rommelmdalisay@gmail.com>
2026-05-06 15:44:13 +0800

    item crud

--- js/item.js
+++ js/item.js
@@ -160,4 +160,55 @@ $(document).ready(function () {
             }
         });
     });
+
+    $('#itable tbody').on('click', 'a.deletebtn', function (e) {
+        e.preventDefault();
+        var table = $('#itable').DataTable();
+        var id = $(this).data('id');
+        var $row = $(this).closest('tr');
+        console.log(id);
+        // if (getToken()) {
+        bootbox.confirm({
+            message: "do you want to delete this item",
+            buttons: {
+                confirm: {
+                    label: 'yes',
+                    className: 'btn-success'
+                },
+                cancel: {
+                    label: 'no',
+                    className: 'btn-danger'
+                }
+            },
+            callback: function (result) {
+                console.log(result);
+                if (result) {
+                    $.ajax({
+                        method: "DELETE",
+                        url: `${url}/api/v1/items/${id}`,
+                        dataType: "json",
+                        // headers: {
+                        //     "Authorization": "Bearer " + getToken()
+                        // },
+                        success: function (data) {
+                            console.log(data);
+                            $row.fadeOut(4000, function () {
+                                table.row($row).remove().draw();
+                            });
+
+                            bootbox.alert(data.success);
+                        },
+                        error: function (error) {
+                            bootbox.alert(data.error);
+                        }
+                    });
+
+                }
+
+            }
+        });
+
+        // }
+
+    })
 })
